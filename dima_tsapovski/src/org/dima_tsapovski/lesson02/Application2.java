eeacaef45d2951d5e52c3cb073166892f04414df
tsapovaki <tsapovaki@Hippy>
2017-04-01 15:51:51 +0300

    lesson

---
+++ dima_tsapovski/src/org/dima_tsapovski/lesson02/Application2.java
@@ -0,0 +1,28 @@
+package org.dima_tsapovski.lesson02;
+
+public class Application2 {
+
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 3;
+		
+		
+		print(x, y);
+		
+		y=x;
+		print(x, y);
+		
+		Integer xx = new Integer (2);
+		Integer yy = new Integer (3);
+
+		
+		print(xx, yy);
+		yy=xx;
+		print(xx, yy);
+	}
+	private static void print(Integer x, Integer y) {
+		System.out.println("x=" + x+",y=" +y);
+	}
+
+}
