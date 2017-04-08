01cf3d93fb27bb23dedccecd2a9b355bb21258b2
HP <HP@Adrenalinka>
2017-04-08 10:12:12 +0300

    lesson2

--- alex_kurets/src/org/alexkurets/lesson2/Application.java
+++ alex_kurets/src/org/alexkurets/lesson2/Application.java
@@ -2,14 +2,14 @@ package org.alexkurets.lesson2;
 
 
 public class Application {
-	/*
+	
 	private static int x;
 	private static int y=1;
 	
 	public void printVariables(){
 		System.out.println(x);
 		System.out.println(y);
-	}*/
+	}
 	
 	public static void main(String[] args) {
 		//Application ap = new Application();
