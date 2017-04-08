af9f9515617777c1f73816cefb04170b57001823
HP <HP@Adrenalinka>
2017-04-08 10:32:28 +0300

    homework2

--- alex_kurets/src/org/alexkurets/homework2/Main.java
+++ alex_kurets/src/org/alexkurets/homework2/Main.java
@@ -2,7 +2,7 @@ package org.alexkurets.homework2;
 
 public class Main {
     public static void main(String [] args){
-        int arrIn[] = {-1,-2,100,-4,-5,-6};
+        int arrIn[] = {1,2,3,3,4,6};
 
         System.out.println(SpecClass1.calcEvenOddNum(arrIn));
         System.out.println(SpecClass2.isSorted(arrIn));
