ebf862ab87e90f82ed2707a0bd1e750401fcd9ea
Alexander Medvedev <amedvedev@iba.by>
2017-04-14 16:13:24 +0300

    examples for lesson 4 - update 1

--- bigroi_samples/src/com/bigroi/samples/execution/PerformanceMeasurer.java
+++ bigroi_samples/src/com/bigroi/samples/execution/PerformanceMeasurer.java
@@ -27,12 +27,12 @@ public class PerformanceMeasurer {
 		System.out.println();
 		System.out.println( "Измеряется " + executable );
 		long millisPassed = this.measure(executable);
-		System.out.println( "Время выполнения: " + millisPassed + " (мсек.)" );
-		System.out.println( "Количество выполнений: " + getExecutionsCount() );
+		System.out.println( "Время выполнения:          " + millisPassed + " (мсек.)" );
+		System.out.println( "Количество выполнений:     " + getExecutionsCount() );
 		return millisPassed;
 	}
 	
-	public long measure(final Executable executable) {		
+	public long measure(final Executable executable) {
 		final TimeMeasurer timeMeasurer = new TimeMeasurer();
 		for (int i = 0; i < executionsCount; i++) {
 			executable.execute();
