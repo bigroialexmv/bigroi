42b492c49153053438ed4af545b55bcf21aebee2
dv6700 <dv6700@192.168.0.104>
2017-04-15 15:48:44 +0300

    lesson04

---
+++ vladimir_yashchenko/src/org/VovaYa/lesson04/Range.java
@@ -0,0 +1,14 @@
+package org.VovaYa.lesson04;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(value=ElementType.FIELD)
+
+public @interface Range {
+	
+	public int min() default 0;
+	public int max() default 100;
+	
+
+}
---
+++ vladimir_yashchenko/src/org/VovaYa/lesson04/WeekEnumSampleApp.java
@@ -0,0 +1,13 @@
+package org.VovaYa.lesson04;
+
+public class WeekEnumSampleApp {
+
+	public static void main(String[] args) {
+		WeekDay day = WeekDay.MONDAY;
+		System.out.println(day.name);
+		System.out.println(day.order);
+		
+
+	}
+
+}
