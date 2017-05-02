78e00569e74699efaa055e0eebe445031aa2548e
Alexander Medvedev <amedvedev@iba.by>
2017-05-02 13:07:22 +0300

    homework 06 updates

--- bigroi_classwork/src/com/bigroi/classwork/lesson06/shop/ProductDaoFileImpl.java
+++ bigroi_classwork/src/com/bigroi/classwork/lesson06/shop/ProductDaoFileImpl.java
@@ -24,19 +24,32 @@ public class ProductDaoFileImpl implements Closeable {
 		}
 	}
 
-	// code 8 bytes
-	// name 100 bytes
-	// price 32 bytes
-	public void saveProduct(Product product) throws DaoException {
+	/**
+	 * Сохраняет объект Product в файл путем создания записи в файле длиной 140 байт. Запись состоит из 3-х частей (полей):
+	 *   - поле code длиной 8 байт 
+	 *   - поле name длиной 100 байт
+	 *   - поле price длиной 32 байт
+	 * @param product
+	 * @throws DaoException
+	 */	
+	public void insertProduct(Product product) throws DaoException {
+		// массив-буфер - заполняем его данными из объекта
+		// code записываем в buffer начиная с индекса 0 и заканчивая индексом 7: [0,7]
+		// name будет храниться в диапазоне индексов 8 и 107 (включая): [108, 140] 
+		// price будет храниться в диапазоне [108, 140]
 		byte[] buffer = new byte[140];
 		
-		String code = product.getCode();		
+		// преобразуем символы поля code в массив байтов
+		String code = product.getCode();
 		byte[] codeBuffer = code.getBytes();
+		// проверяем длину массива; если превосходит 8, то выбрсываем исключение
 		if (codeBuffer.length > 8) {
 			throw new DaoException("Could not write to file");
 		}
+		// копируем все байты из codeBuffer 
 		System.arraycopy(codeBuffer, 0, buffer, 0, codeBuffer.length);
 		
+		// записываем name в buffer
 		String name = product.getName();
 		byte[] nameBuffer = name.getBytes();
 		if (nameBuffer.length > 100) {
@@ -44,7 +57,10 @@ public class ProductDaoFileImpl implements Closeable {
 		}
 		System.arraycopy(nameBuffer, 0, buffer, 8, nameBuffer.length);
 		
+		
+		// записываем price в buffer
 		BigDecimal price = product.getPrice();
+		// price для простоты преобразовываем сначала в строку, затем в byte-массив
 		byte[] priceBuffer = price.toString().getBytes();
 		if (nameBuffer.length > 32) {
 			throw new DaoException("Could not write to file");
@@ -52,6 +68,7 @@ public class ProductDaoFileImpl implements Closeable {
 		System.arraycopy(priceBuffer, 0, buffer, 108, priceBuffer.length);
 		
 		try {
+			// пишем наш buffer в файл
 			dataFile.write(buffer);
 		} catch (IOException e) {
 			throw new DaoException("Could not write to file", e);
@@ -59,16 +76,27 @@ public class ProductDaoFileImpl implements Closeable {
 		
 	}
 	
+	/**
+	 * Читает запись из файла с номером n; прочитанные поля помещает в новый объект product
+	 * @param n
+	 * @return - product
+	 * @throws DaoException
+	 */
 	public Product getProductByRecordNo(int n) throws DaoException {
-		Product product = null;//new Product();
+		Product product = null;
 		try {
+			// n * 140 - индекс байта, начиная с которого будем считывать поля из файла			 
 			dataFile.seek(n * 140);
 			
+			// читаем 1-ые 8 байт в массив codeBuffer
 			byte[] codeBuffer = new byte[8];
 			dataFile.read(codeBuffer);
+			// преобразуем массив codeBuffer в строку
 			String code = new String(codeBuffer);
+			// удаляем пробелы слева / справа
 			code = code.trim();
 			
+			// читаем следующие 100 байт и т.д.
 			byte[] nameBuffer = new byte[100];
 			dataFile.read(nameBuffer);
 			String name = new String(nameBuffer).trim();
@@ -84,10 +112,10 @@ public class ProductDaoFileImpl implements Closeable {
 		return product;
 	}
 	
-	public Product getProductByCode(String code) throws DaoException {
-		Product product = null;//new Product();
-		return product;
-	}
+//	public Product getProductByCode(String code) throws DaoException {
+//		Product product = null;//new Product();
+//		return product;
+//	}
 
 	@Override
 	public void close() {
