fc28433f06239e915f60fcdcf94dd7afac8fedfb
Alexander Medvedev <amedvedev@iba.by>
2017-05-16 16:30:36 +0300

    comments added for lesson 07

--- bigroi_classwork/src/com/bigroi/classwork/lesson07/WaitNotifySampleApp.java
+++ bigroi_classwork/src/com/bigroi/classwork/lesson07/WaitNotifySampleApp.java
@@ -15,7 +15,9 @@ class Worker implements Runnable {
 		while (true) {
 			try {
 				System.out.println("Waiting for notification");
+				// помещаем waitNotify.notify() в synchronized-блок 
 				synchronized (waitNotify) {
+					// текущий поток ждет, пока поток-main не вызовет waitNotify.notify() 
 					waitNotify.wait();
 					System.out.println("Piece of work done");
 				}
@@ -35,6 +37,9 @@ public class WaitNotifySampleApp {
 		
 		Worker worker = new Worker(waitNotify);
 		Thread workerThread = new Thread(worker);
+		// создаем поток-демон путем установки свойства deamon в значение true
+		// (по умолчанию deamon = false )
+		// поток-демон завершит выполнение по окончанию работы основного потока main
 		workerThread.setDaemon(true);
 		workerThread.start();
 		
@@ -50,7 +55,9 @@ public class WaitNotifySampleApp {
 			if( "q".equals( cmd ) ) {
 				break;
 			} else if( "n".equals( cmd ) ) {
+				// согласно Java-документации помещаем waitNotify.notify() в synchronized-блок 
 				synchronized (waitNotify) {
+					// сообщаем потоку workerThread, что он может продолжить выполнение после waitNotify.wait()
 					waitNotify.notify();					
 				}
 				System.out.println("notified");
