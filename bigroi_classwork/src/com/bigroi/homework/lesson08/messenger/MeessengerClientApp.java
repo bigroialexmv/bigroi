f30d91b52966c29ef82abc1c8a0393540f2635eb
User <User@User_Home>
2017-05-20 12:41:09 +0200

    samples for 08

--- bigroi_classwork/src/com/bigroi/homework/lesson08/messenger/MeessengerClientApp.java
+++ bigroi_classwork/src/com/bigroi/homework/lesson08/messenger/MeessengerClientApp.java
@@ -1,21 +1,60 @@
 package com.bigroi.homework.lesson08.messenger;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
+
+class ClientServer implements Runnable {
+	
+	private Socket socket;
+	
+	private DataInputStream dis;
+
+	public ClientServer(Socket socket) throws IOException {
+		super();
+		this.socket = socket;
+		this.dis = new DataInputStream( socket.getInputStream() );
+	}
+
+	@Override
+	public void run() {		
+		while(true) {
+			try {
+				String message = dis.readUTF();
+				System.out.println(message);
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
+		}
+	}	
+	
+}
+
+
 public class MeessengerClientApp {
 
 	public static void main(String[] args) {
 		Socket socket = null;
 		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Your name: ");
+		String name = scanner.nextLine();
 
 		try {
 			socket = new Socket("192.168.0.100", 9988);
+			
+			ClientServer server = new ClientServer(socket);
+			
+			Thread thread = new Thread(server);
+			thread.start();
+			
 			OutputStream os = socket.getOutputStream();
 			DataOutputStream dos = new DataOutputStream(os);
+			dos.writeUTF(name);
 			
 			while(true) {
 				String message = scanner.nextLine();				
--- bigroi_classwork/src/com/bigroi/homework/lesson08/messenger/MeessengerServerApp.java
+++ bigroi_classwork/src/com/bigroi/homework/lesson08/messenger/MeessengerServerApp.java
@@ -1,30 +1,55 @@
 package com.bigroi.homework.lesson08.messenger;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class ServerClient implements Runnable {
 	
 	private Socket socket;
 	
-	public ServerClient(Socket socket) {
+	private String name;
+	
+	private DataOutputStream dos;
+	
+	private MessageListener messageListener;
+	
+	public ServerClient(Socket socket, MessageListener messageListener) {
 		this.socket = socket;
+		this.messageListener = messageListener;
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	public DataOutputStream getOutputStream() throws IOException {
+//		if (dos == null) {
+//			dos = new DataOutputStream( socket.getOutputStream() );
+//		}
+		return new DataOutputStream( socket.getOutputStream() );
+	}
+
 	@Override
 	public void run() {
 		try {
 			InputStream is = socket.getInputStream();		
 			DataInputStream dis = new DataInputStream(is);
+			name = dis.readUTF();
+			System.out.println(name + " connected");
 			while (true) {
 				String message = dis.readUTF();				// читаем сообщение от клиента
-				System.out.println("Recieved: " + message);
+				System.out.printf("Message recieved from %s: %s\n", name, message);
 				if ("quit".equals(message)) {				// если клиент отправил quit выходим из цикла
 					break;
 				}
+				messageListener.onMessage(this, message);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -32,18 +57,44 @@ class ServerClient implements Runnable {
 	}
 }
 
-public class MeessengerServerApp {
+interface MessageListener {
+	
+	void onMessage(ServerClient from, String message);
+	
+}
 
-	public static void main(String[] args) {
+class Server implements MessageListener {
+	
+	private List<ServerClient> clients =
+			Collections.synchronizedList( new ArrayList<ServerClient>() );
+	
+	public void onMessage(ServerClient from, String message) {	
+		for(ServerClient client : clients) {
+			if ( client.equals(from) ) continue;
+			try {
+				System.out.println("Sending message to " + client.getName());
+				DataOutputStream dos = client.getOutputStream();
+				String msg = String.format("%s: %s", from.getName(), message);
+				dos.writeUTF(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void start() {		
+		
 		System.out.println("Server started");
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(9988);			
+			serverSocket = new ServerSocket(9988);
 			while(true) {
 				Socket socket = serverSocket.accept();
 				System.out.println( "Client connected: " + socket.getInetAddress() );
 			
-				ServerClient client = new ServerClient(socket);
+				ServerClient client = new ServerClient(socket, this);
+				clients.add(client);
+				
 				Thread thread = new Thread(client);
 				thread.start();
 			}		
@@ -59,6 +110,14 @@ public class MeessengerServerApp {
 			}
 		}
 		System.out.println("Server stopped");
+	}
+}
+
+public class MeessengerServerApp {	
+	
+	public static void main(String[] args) {		
+		Server server = new Server();
+		server.start();
 	}
 
 }
