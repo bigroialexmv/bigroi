bb83dc7b33dd1a93aa50cae1c3caa18c459ae85b
нр615 <615@192.168.0.106>
2017-05-27 14:49:18 +0300

    sample

--- bigroi_shop_web1/src/com/bigroi/shop/web/ProductServlet.java
+++ bigroi_shop_web1/src/com/bigroi/shop/web/ProductServlet.java
@@ -30,6 +30,8 @@ public class ProductServlet extends HttpServlet {
         super();
         codeProductMap.put("001", new Product("001", "Nokia", 200));
         codeProductMap.put("002", new Product("002", "iPhone", 500));
+        codeProductMap.put("003", new Product("003", "HTC", 400));
+        codeProductMap.put("004", new Product("004", "Sumsung", 300));
         System.out.println("ProductServlet created");
     }
 
@@ -39,42 +41,39 @@ public class ProductServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
 		writer.append(
-"<!DOCTYPE html>"+
-"<html>"+
-"<head>"+
-"<meta charset=\"UTF-8\"/>"+
-"<title>E-Shop</title>"+
-"</head>"+
-"<body>"+
-"	<h3>Welcome to E-Shop</h3>"+
-"	<table style=\"width: 100%; \">"+
-"		<caption style=\"font-weight: bold;\">Products</caption>"+
-"		<thead>"+
-"			<tr>"+
-"				<th style=\"text-align: left;\">Code</th>"+
-"				<th style=\"text-align: left;\">Name</th>"+
-"				<th style=\"text-align: right;\">Price</th>"+
-"			</tr>"+
-"		</thead>"+
-"		<tbody>"+
-"			<tr>"+
-"				<td>001</td>"+
-"				<td>"+
-"					<a href=\"/bigroi_shop_web1/nokia.html\">Nokia</a>"+
-"				</td>"+
-"				<td style=\"text-align: right;\">200</td>"+
-"			</tr>"+
-"			<tr>"+
-"				<td style=\"border-bottom: 1px; border-color: gray;\">002</td>"+
-"				<td>iPhone</td>"+
-"				<td style=\"text-align: right;\">500</td>"+
-"			</tr>"+
-"		</tbody>"+
-"	</table>	"+
-"</body>"+
-"</html>"
-	
-);
+				"<!DOCTYPE html>"+
+						"<html>"+
+						"<head>"+
+						"<meta charset=\"UTF-8\"/>"+
+						"<title>E-Shop</title>"+
+						"</head>"+
+						"<body>"+
+						"	<h3>Welcome to E-Shop</h3>"+
+						"	<table style=\"width: 100%; \">"+
+						"		<caption style=\"font-weight: bold;\">Products</caption>"+
+						"		<thead>"+
+						"			<tr>"+
+						"				<th style=\"text-align: left;\">Code</th>"+
+						"				<th style=\"text-align: left;\">Name</th>"+
+						"				<th style=\"text-align: right;\">Price</th>"+
+						"			</tr>"+
+						"		</thead>"+
+					"		<tbody>");
+		for(Product p : codeProductMap.values()) {
+			writer.append(
+					"			<tr>"+
+					"				<td>" + p.getCode() + "</td>"+
+					"				<td>"+
+					"					<a href=\"/bigroi_shop_web1/nokia.html\">" + p.getName() + "</a>"+
+					"				</td>"+
+					"				<td style=\"text-align: right;\">" + p.getPrice() + "</td>"+
+					"			</tr>");
+		}
+		writer.append(			
+				"		</tbody>"+
+				"	</table>	"+
+				"</body>"+
+				"</html>");
 	}
 
 	/**
