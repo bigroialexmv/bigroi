35380377587dfccafb5b4f08f8f76e75b35c7a70
нр615 <615@192.168.0.100>
2017-06-10 14:00:47 +0300

    jdbc sample

--- bigroi_shop_web1/src/com/bigroi/shop/db/MySqlTest.java
+++ bigroi_shop_web1/src/com/bigroi/shop/db/MySqlTest.java
@@ -2,6 +2,11 @@ package com.bigroi.shop.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.bigroi.shop.model.Product;
 
 /*
 Host: sql11.freesqldatabase.com
@@ -12,11 +17,39 @@ Port number: 3306
  */
 
 public class MySqlTest {
+	
 
-	public static void main(String[] args) throws Exception {
-		Connection conn =
-			       DriverManager.getConnection("jdbc:mysql://sql11.freesqldatabase.com:3306/sql11179471?" +
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;	
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://sql11.freesqldatabase.com:3306/sql11179471?" +
 			                                   "user=sql11179471&password=XRvjw6EMj1");
+			stmt = conn.createStatement();
+			String sql = "SELECT CODE, NAME, PRICE FROM PRODUCT";
+			ResultSet rs = stmt.executeQuery(sql);
+			while ( rs.next() ) {
+				int code = rs.getInt("CODE");
+				String name = rs.getString("NAME");
+				double price = rs.getDouble("PRICE");
+				Product p = new Product(new Integer(code).toString(), name, price, null);
+				System.out.println(p);
+			}
+			rs.close();		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null) stmt.close();
+			} catch (SQLException e) {				
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null) conn.close();
+			} catch (SQLException e) {				
+				e.printStackTrace();
+			}
+		}
 		
 
 	}
