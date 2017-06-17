c4585da9415e5a4f91792e8739eef8973bc72b9e
нр615 <615@192.168.0.100>
2017-06-17 16:16:47 +0300

    sqls

---
+++ bigroi_shop_database/Scripts/user_201706171609.sql
@@ -0,0 +1,7 @@
+INSERT INTO bigroi_shop.`user` (USER_ID,FIRST_NAME,LAST_NAME,EMAIL,CRTD_TMS,UPDT_TMS,PHONE) VALUES 
+(18,'Alexander','Medvedev','alex-medvedev@tut.by','2017-06-17 14:17:29.000','2017-06-17 14:19:16.000','+375297505384')
+,(19,'SERGEY','TSAPOVSKI','TSAPOVSKI@YANDEX.RU','2017-06-17 14:20:39.000','2017-06-17 14:20:39.000','102')
+,(20,'Sergey','Morozov','smarozau@gmail.com','2017-06-17 14:21:47.000','2017-06-17 14:21:47.000','+375295566078')
+,(22,'Dmitry','Tsapovski','tsapovski@gmail.com','2017-06-17 14:22:52.000','2017-06-17 14:22:52.000','+375296592016')
+,(24,'Vladimir','Yashchenko','yashchenko-vladimir@tut.by','2017-06-17 14:23:57.000','2017-06-17 14:23:57.000','+375295773281')
+;
---
+++ bigroi_shop_database/Scripts/user_address_201706171610.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO bigroi_shop.user_address (ADDR_ID,STREET_ADDR,CITY,USER_ID) VALUES 
+(4,'ul.Kalvariyskaya, 25','Minsk',20)
+,(5,'ul. J. Kolasa, 1','Minsk',18)
+,(6,'ul. Fr. Skorinu, 36','Minsk',24)
+,(7,'BULVAR SHAHTIOROV','SOLIGORSK',19)
+,(8,'BULVAR SHAHTIOROV','SOLIGORSK',19)
+,(9,'BULVAR SHAHTIOROV','SOLIGORSK',19)
+,(10,'BULVAR SHAHTIOROV','SOLIGORSK',19)
+,(11,'ul. bogomolova','Solihorsk',22)
+;
